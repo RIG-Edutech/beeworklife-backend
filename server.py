09e792fbb792e92a18c5c8d3097a4298b12e4618
Jason <JS22-1@slc.net>
2023-09-14 13:47:14 +0700

    Update server.py for heroku

--- server.py
+++ server.py
@@ -177,4 +177,6 @@ def check_user_id(user_id):
     return
 
 if __name__=='__main__':
-    app.run(debug=True,use_reloader=False, port=os.environ.get('FLASK_RUN_PORT'))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port = port)
+    # app.run(debug=True,use_reloader=False, port=os.environ.get('FLASK_RUN_PORT'))
